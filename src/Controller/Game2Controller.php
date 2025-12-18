b3e49bcbf581288e169b41ab3a63035dcc68ad27
AngelCieep <angelmaribla777@digitechfp.com>
2025-12-18 09:46:06 +0100

    tetris:)

--- src/Controller/Game2Controller.php
+++ src/Controller/Game2Controller.php
@@ -11,8 +11,11 @@ final class Game2Controller extends AbstractController
     #[Route('/game2', name: 'app_game2')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        
         return $this->render('game2/index.html.twig', [
             'controller_name' => 'Game2Controller',
+            'user' => $user,
         ]);
     }
 }
