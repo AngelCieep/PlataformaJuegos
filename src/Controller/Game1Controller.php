eb11462d29266d03b734af69470ffb6fa43f9584
AngelCieep <angelmaribla777@digitechfp.com>
2026-01-08 11:21:30 +0100

    correccion txus

--- src/Controller/Game1Controller.php
+++ src/Controller/Game1Controller.php
@@ -2,22 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Juego;
+use App\Repository\JuegoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class Game1Controller extends AbstractController
 {
-    #[Route('/game1', name: 'app_game1')]
-    public function index(): Response
+    #[Route('/juego/{id}', name: 'app_juegos')]
+    public function index(int $id, JuegoRepository $juegoRepository): Response
     {
+        // Buscar el juego en la base de datos por ID
+        $juego = $juegoRepository->find($id);
+        
+        // Si el juego no existe o no está activo, redirigir a la plataforma de juegos
+        if (!$juego || !$juego->isEstado()) {
+            $this->addFlash('error', 'El juego solicitado no está disponible.');
+            return $this->redirectToRoute('app_game_platform');
+        }
+        
         $user = $this->getUser();
 
-        return $this->render('game1/index.html.twig', [
-            'controller_name' => 'Game1Controller',
+        return $this->render('juegos/juego'.$id.'.html.twig', [
             'user' => $user,
-            'api_key' => 'ABCDEFGHIJK1234567890',
-            'game_token' => 'SNAKE_GAME_TOKEN_001',
+            'juego' => $juego,
+            'api_key' => $juego->getAplicacion()->getApiKey(),
+            'game_token' => $juego->getTokenJuego(),
         ]);
     }
 }
